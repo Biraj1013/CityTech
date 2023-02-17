ceeff0a4946c19823eeff9aceffb34a33f2ac032
VirajRS1013 <biraj.ghimire@riddhasoft.com>
2023-02-17 15:01:18 +0545

    final commit

--- lib/presentation/constants/colors.dart
+++ lib/presentation/constants/colors.dart
@@ -4,8 +4,5 @@ class AppColor {
   // static const primaryColor = Color.fromARGB(255, 27, 41, 61);
   static const primaryColor = Color(0xFF1C2757);
   static const amberAccent = Color.fromARGB(217, 250, 187, 0);
-  static const backgroundColor = Color.fromARGB(255, 239, 236, 236);
   static const white = Colors.white;
-  static const textColor = Colors.black87;
-  static const inActiveButtonColor = Color.fromARGB(255, 153, 153, 153);
 }
